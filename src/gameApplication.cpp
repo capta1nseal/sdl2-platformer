bade2e89e3252ab105557338997f629d19160f76
capta1nseal <capta1nseal@psil.fi>
2023-04-19 16:50:52 +0300

    tune debug out

--- src/gameApplication.cpp
+++ src/gameApplication.cpp
@@ -67,7 +67,7 @@ public:
 
             draw();
 
-            int accuracy = (int) (targetRefreshTime.count() * 0.000001 - delta.count()) * 100000000;
+            int accuracy = (int) ((targetRefreshTime.count() * 0.000001 - delta.count()) * 100000000) ;
             for (int i = 0; i < accuracy; i++)
             {
                 cout << " ";
